78d97238cf204254b6caf8b809d9f84d7da08375
FAHAD <eng.bokhari@hotmail.com>
2024-09-29 19:39:04 +1300

    Create main script

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+import numpy as np
+#from deap import base, creator, tools, algorithms
+import pandas
+import pandas as pd
+import os
+
+
+def main():
+    print()
+
+if __name__ == "__main__":
+    main()
